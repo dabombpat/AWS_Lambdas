358a5f24ca21f5822ef97bf79540aa88ca846d69
EC2 Default User <ec2-user@ip-172-31-68-7.ec2.internal>
2022-11-30 22:12:53 +0000

    Updated Lambda to reject anything but patflan

--- ALambda/hello-world/app.js
+++ ALambda/hello-world/app.js
@@ -5,7 +5,7 @@ let response;
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
+ * @pmaram {Object} event - API Gateway Lambda Proxy Input Format
  *
  * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
  * @param {Object} context
@@ -15,9 +15,28 @@ let response;
  * 
  */
 exports.lambdaHandler = async (event, context) => {
+    let status;
+    let body = {};
+    
     try {
-        
+        console.log(event);
+        let actual_event = event.body;
+        let info = JSON.parse(actual_event);
         let number = 1;
+        
+        let email = event.arg1;
+        let password = event.arg2;
+        
+        if(email == "patflan"){
+            if (password == flan){
+                status = 200
+            }        
+            
+        } else{
+            status = 400;
+            body["Incorrect Username or Password"]
+        }
+        
         // const ret = await axios(url);
         response = {
             'statusCode': 200,
@@ -28,8 +47,21 @@ exports.lambdaHandler = async (event, context) => {
         }
     } catch (err) {
         console.log(err);
-        return err;
+        status = 400;
+        body["error"] = err.toString();
     }
+    
+        response = {
+        'statusCode': status,
+        
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin" : "'", // Allow from anywhere
+            "Access-Control-Allow-Methods" : "POST" // Allow Post request
+        },
+        
+        'body' : JSON.stringify(body)
+    };
 
     return response
 };
