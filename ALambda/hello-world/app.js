7de5554c6c1e5945adf6028af8a6fe48fb3abd23
EC2 Default User <ec2-user@ip-172-31-68-7.ec2.internal>
2022-12-01 07:35:16 +0000

    lambda registering user

--- ALambda/hello-world/app.js
+++ ALambda/hello-world/app.js
@@ -1,6 +1,29 @@
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
 let response;
+const mysql = require('mysql');
+
+var config = require('./config.json');
+var pool = mysql.createPool({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+});
+
+
+function query(conx, sql, params){
+    return new Promise((resolve, reject) => {
+        conx.query(sql, params, function(err, rows){
+            if(err){
+                reject(err);
+            }else{
+                resolve(rows);
+            }
+        });
+    });
+}
+
 
 /**
  *
@@ -15,17 +38,19 @@ let response;
  * 
  */
  
+ 
 exports.lambdaHandler = async (event, context) => {
     let status = 400;
     let body = {};
     var userpasswords = {};
+    console.log("Login or Register!")
     
     try {
-        console.log(event);
+        //console.log(event);
         let actual_event = event.body;
-        console.log(actual_event)
+        //console.log(actual_event)
         let info = JSON.parse(actual_event);
-        console.log(info)
+        //console.log(info)
         
         let email = info['arg1'];
         let password = info['arg2'];
@@ -34,7 +59,74 @@ exports.lambdaHandler = async (event, context) => {
         console.log("password : ", password)
         console.log("LoginOrRegister : ", LoginOrRegister)
         
+        let ListDesigners = (username) => {
+            console.log('gets hungup here?');
+            return new Promise((resolve,reject) => {
+                pool.query("SELECT * FROM Register WHERE username=?", [username], (error, rows) => {
+                    if(error) {return reject("Unable to list Projects");}
+                    else {
+                        return resolve(rows);
+                    }
+                });
+            })}
+            
+        let InsertNewDesigner = (username, password) => {
+            console.log("inserting new designer");
+            return new Promise((resolve,reject) => {
+                pool.query("INSERT INTO Register (username,password) VALUES(?,?)", [username, password], (error, rows) => {
+                    if(error) {return reject("Unable to list Projects");}
+                    else {
+                        console.log("Sucess");
+                        return resolve(true);
+                    }
+                });
+            });}    
+            
+            
+        try {
+            console.log('how about here?');
+            if(LoginOrRegister == 'R'){
+                console.log("LoR = R");
+                const registereddesigner = await InsertNewDesigner(email, password);
+                console.log("finished inserting new designer");
+            }
+            
+            if(LoginOrRegister == 'L'){
+                const designers = await ListDesigners(info.username);
+                if(designers.length != 0) {
+                    response.statusCode=  200;
+                    response.result = designers ;
+                } else {
+                    response.statusCode = 400;
+                    response.error = "There are no registered designers with this username";
+                }
+            }    
+                
+
+        } catch (error) {
+            response.statusCode = 400;
+            response.error = error;
+        }
         
+        //console.log(response);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //------------------------------------Below only logs into patflan --------
         // if(email == "patflan"){
         //     if (password == "flan"){
         //         status = 200;
@@ -46,47 +138,54 @@ exports.lambdaHandler = async (event, context) => {
         //     body["error"] = "Incorrect Username or Password";
         // }
         
-        if(LoginOrRegister == 'L'){
-            console.log("attempting to log in")
-            for(var username in userpasswords) {
-              var passkey = userpasswords[username];
-                if(username == email){
-                    if(passkey == password){
-                        status = 200;
-                        body["result"] = "Found Username and Password"
-                    }
-                }
-            }
-        }
+        //---------------------------------------------------Only can register users ------------
         
-        if(LoginOrRegister == 'R'){
-            console.log("attempting to Register")
-            for(var username in userpasswords) {
-              var passkey = userpasswords[username];
-                if(username == email){
-                    console.log("Already Registered")
-                    status = 403;
-                    body["result"] = "Already Registered"
+        // if(LoginOrRegister == 'L'){
+        //     console.log("attempting to log in")
+        //     for(var username in userpasswords) {
+        //       var passkey = userpasswords[username];
+        //         if(username == email){
+        //             if(passkey == password){
+        //                 status = 200;
+        //                 body["result"] = "Found Username and Password"
+        //             }
+        //         }
+        //     }
+        // }
+        
+        // if(LoginOrRegister == 'R'){
+        //     console.log("attempting to Register")
+        //     for(var username in userpasswords) {
+        //       var passkey = userpasswords[username];
+        //         if(username == email){
+        //             console.log("Already Registered")
+        //             status = 403;
+        //             body["result"] = "Already Registered"
                     
-                        response = {
-                            'statusCode': status,
+        //                 response = {
+        //                     'statusCode': status,
                             
-                            headers: {
-                                "Access-Control-Allow-Headers" : "Content-Type",
-                                "Access-Control-Allow-Origin" : "'", // Allow from anywhere
-                                "Access-Control-Allow-Methods" : "POST" // Allow Post request
-                            },
+        //                     headers: {
+        //                         "Access-Control-Allow-Headers" : "Content-Type",
+        //                         "Access-Control-Allow-Origin" : "'", // Allow from anywhere
+        //                         "Access-Control-Allow-Methods" : "POST" // Allow Post request
+        //                     },
                             
-                            'body' : JSON.stringify(body)
-                        };
-                    return response
+        //                     'body' : JSON.stringify(body)
+        //                 };
+        //             return response
 
-                }
-            }
-            userpasswords[email] = password;
-            status = 200;
-            body["result"] = "Found Username and Password"
-        }
+        //         }
+        //     }
+        //     userpasswords[email] = password;
+        //     status = 200;
+        //     body["result"] = "Found Username and Password"
+        // }
+
+// -----------------------------------------------------------------------------
+
+
+
 
     } catch (err) {
         console.log(err);
